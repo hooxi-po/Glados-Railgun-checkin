379396a5888eaf57bb9301f086504614092f8e9b
O2O <liujiantao7000@163.com>
2026-08-03 09:46:08 +0800

    fix: use active GLaDOS domain

--- checkin.py
+++ checkin.py
@@ -106,7 +106,7 @@ class Config:
     DEFAULT_VERBOSE = False
 
     """默认域名"""
-    DOMAINS = ["glados.cloud", "railgun.info", "glados-facility.com"]
+    DOMAINS = ["glados-facility.com"]
 
     """兑换计划列表"""
     EXCHANGE_PLANS = {
